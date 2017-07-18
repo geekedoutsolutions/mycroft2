8a43914b566d386b774890cb0c8e8b83b4dfe763
geekedoutsolutions <geekedoutsolutions@gmail.com>
2017-07-18 12:12:43 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,10 +14,9 @@ class PersonalitySkill1(MycroftSkill):
         super(PersonalitySkill1, self).__init__(name="PersonalitySkill1")
 		
 	def initialize(self):
-        matt_or_dave_intent = IntentBuilder("MattOrDaveIntent").\
-            require("MattOrDaveKeyword").build()
-        self.register_intent(matt_or_dave_intent,
-                             self.handle_matt_or_dave_intent)
+             matt_or_dave_intent = IntentBuilder("MattOrDaveIntent").\
+                 require("MattOrDaveKeyword").build()
+            self.register_intent(matt_or_dave_intent, self.handle_matt_or_dave_intent)
 	
 	def handle_matt_or_dave_intent(self, message):
         self.speak_dialog("matt.dave")
