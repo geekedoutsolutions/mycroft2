c3364bd19eb8e25f1a4c8486992eb225541e20a9
chris-mcawesome12 <chrismccann12@yahoo.ca>
2017-07-18 11:37:59 -0400

    Add files via upload

---
+++ _init_.py
@@ -0,0 +1,30 @@
+from os.path import dirname
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+
+__author__ = 'chrismcawesome'
+
+LOGGER = getLogger(__name__)
+
+class PersonalitySkill1(MycroftSkill):
+
+    def __init__(self):
+        super(PersonalitySkill1, self).__init__(name="PersonalitySkill1")
+		
+	def initialize(self):
+        matt_ordave_intent = IntentBuilder("MattOrDaveIntent").\
+            require("MattOrDaveKeyword").build()
+        self.register_intent(matt_or_dave_intent,
+                             self.handle_matt_or_dave_intent)
+	
+	def handle_matt_or_dave_intent(self, message):
+        self.speak_dialog("matt.dave")
+	
+	
+	def stop(self):
+        pass
+	
+def create_skill():
+    return PersonalitySkill1()
